02d1ef38bd59b9d867c11330584a3bf910d77a09
Florexiz <florexiz.nm@gmail.com>
2021-08-15 17:08:37 +0300

    Task

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,14 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld hello1 = applicationContext.getBean("helloworld", HelloWorld.class);
+        HelloWorld hello2 = applicationContext.getBean("helloworld", HelloWorld.class);
+
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println(hello1 == hello2);
+        System.out.println(cat1 == cat2);
     }
 }
